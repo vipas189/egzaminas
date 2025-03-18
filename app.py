d9dd863117d32c0314971478e208f0313f0b5b9a
vipas189 <rokx987@gmail.com>
2025-03-18 15:35:46 +0200

    Init

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ from extensions import db, migrate
 from routes.__init__ import routes
 from models.users import Users
 
-
-def main():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)
-    migrate.init_app(app, db)
-    routes(app)
-
-    app.run(debug=True)
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+migrate.init_app(app, db)
+routes(app)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
