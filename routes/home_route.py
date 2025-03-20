1caf29b2eaaaf4b4dc10ccf8d2a8eb79587ffa8e
Dom4z <dominykaz123@gmail.com>
2025-03-20 16:07:59 +0200

    Pataisytas kodas, parašyta html pasibandimo dalis

--- routes/home_route.py
+++ routes/home_route.py
@@ -1,7 +1,8 @@
 from flask import render_template
 
 
-def home_route(app):
-    @app.route("/")
-    def home():
-        return render_template("home.html")
+# def home_route(app):
+#     @app.route("/test")
+#     def home():
+#         return render_template("index.html")
+
