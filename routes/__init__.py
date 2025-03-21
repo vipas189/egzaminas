ffc547bc9d4786dc920d1bb7368eb5adc8e292bd
Milda Samoskaite <mildasamo@gmail.com>
2025-03-21 17:40:06 +0200

    haha

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,17 +1,10 @@
 from routes.home_route import home_route
 from routes.login_route import login_route
-<<<<<<< HEAD
 from routes.panel_route import panel_route
-=======
-from routes.register_route import register_route
->>>>>>> 9dd53898c0cb4364742b788cbdb03a3944733450
 
 
 def routes(app):
     home_route(app)
     login_route(app)
     panel_route(app)
-<<<<<<< HEAD
    
-=======
->>>>>>> 9dd53898c0cb4364742b788cbdb03a3944733450
