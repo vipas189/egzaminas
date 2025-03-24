fb6d089f3575c1cd4cccfacd180f0233501aa22f
vipas189 <rokx987@gmail.com>
2025-03-24 17:13:52 +0200

    BLOCK USER ON BAD LOGIN ATTEMPT

--- routes/login_route.py
+++ routes/login_route.py
@@ -3,7 +3,12 @@ from flask_login import login_user
 from models.form.login_form import LoginForm
 from werkzeug.security import check_password_hash
 from config import Config
-from services.user_login_services import user_exists
+from services.user_login_services import (
+    user_exists,
+    user_email_exists,
+    user_failed_login,
+    user_blocked,
+)
 from models.users import Users
 
 
@@ -19,10 +24,24 @@ def login_route(app):
     @app.route("/login/student", methods=["POST"])
     def login_student():
         form = LoginForm()
+        if user_blocked(form.email.data, "student"):
+            flash(
+                "Jusu paskyra buvo uzblokuota 1 minute", category="student_login_error"
+            )
+            flash("student", category="active_tab")
+            return redirect(url_for("login"))
         student = user_exists(form.email.data, form.password.data, "student")
         if student:
             login_user(student)
             return redirect(url_for("panel_student"))
+        elif user_email_exists(form.email.data, "student"):
+            if user_failed_login(form.email.data, "student") >= 3:
+                flash(
+                    "Jusu paskyra buvo uzblokuota 1 minute",
+                    category="student_login_error",
+                )
+                flash("student", category="active_tab")
+                return redirect(url_for("login"))
         flash("Neteisingas el. paštas arba slaptažodis", category="student_login_error")
         flash("student", category="active_tab")
         return redirect(url_for("login"))
@@ -30,10 +49,24 @@ def login_route(app):
     @app.route("/login/lecturer", methods=["POST"])
     def login_lecturer():
         form = LoginForm()
+        if user_blocked(form.email.data, "lecturer"):
+            flash(
+                "Jusu paskyra buvo uzblokuota 1 minute", category="lecturer_login_error"
+            )
+            flash("lecturer", category="active_tab")
+            return redirect(url_for("login"))
         lecturer = user_exists(form.email.data, form.password.data, "lecturer")
         if lecturer:
             login_user(lecturer)
             return redirect(url_for("panel_lecturer"))
+        elif user_email_exists(form.email.data, "lecturer"):
+            if user_failed_login(form.email.data, "lecturer") >= 3:
+                flash(
+                    "Jusu paskyra buvo uzblokuota 1 minute",
+                    category="lecturer_login_error",
+                )
+                flash("lecturer", category="active_tab")
+                return redirect(url_for("login"))
         flash(
             "Neteisingas el. paštas arba slaptažodis", category="lecturer_login_error"
         )
@@ -43,6 +76,10 @@ def login_route(app):
     @app.route("/login/admin", methods=["POST"])
     def login_admin():
         form = LoginForm()
+        if user_blocked(form.email.data, "admin"):
+            flash("Jusu paskyra buvo uzblokuota 1 minute", category="admin_login_error")
+            flash("admin", category="active_tab")
+            return redirect(url_for("login"))
         if form.email.data == Config.ADMIN_EMAIL and check_password_hash(
             Config.ADMIN_PASSWORD_HASH, form.password.data
         ):
@@ -60,6 +97,14 @@ def login_route(app):
         if admin:
             login_user(admin)
             return redirect(url_for("panel_admin"))
+        elif user_email_exists(form.email.data, "admin"):
+            if user_failed_login(form.email.data, "admin") >= 3:
+                flash(
+                    "Jusu paskyra buvo uzblokuota 1 minute",
+                    category="admin_login_error",
+                )
+                flash("lecturer", category="active_tab")
+                return redirect(url_for("login"))
         flash("Neteisingas el. paštas arba slaptažodis", category="admin_login_error")
         flash("admin", category="active_tab")
         return redirect(url_for("login"))
--- services/user_login_services.py
+++ services/user_login_services.py
@@ -1,6 +1,7 @@
 from models.users import Users, db
 from werkzeug.security import check_password_hash
 from sqlalchemy import and_
+from datetime import datetime, timedelta
 
 
 def user_exists(email, password, role):
@@ -8,4 +9,35 @@ def user_exists(email, password, role):
         db.select(Users).filter(and_(Users.email == email, Users.role == role))
     ).scalar()
     if user and check_password_hash(user.password, password):
+        user.login_attempts = 0
+        db.session.commit()
         return user
+
+
+def user_email_exists(email, role):
+    user = db.session.execute(
+        db.select(Users).filter(and_(Users.email == email, Users.role == role))
+    ).scalar()
+    return user
+
+
+def user_failed_login(email, role):
+    user = db.session.execute(
+        db.select(Users).filter(and_(Users.email == email, Users.role == role))
+    ).scalar()
+    user.last_failed_login = datetime.now()
+    user.login_attempts += 1
+    db.session.commit()
+    if user.login_attempts >= 3:
+        user.suspended_until = user.last_failed_login + timedelta(minutes=1)
+        db.session.commit()
+        return user.login_attempts
+    return user.login_attempts
+
+
+def user_blocked(email, role):
+    user = db.session.execute(
+        db.select(Users).filter(and_(Users.email == email, Users.role == role))
+    ).scalar()
+    if (user and user.suspended_until) and (datetime.now() < user.suspended_until):
+        return True
